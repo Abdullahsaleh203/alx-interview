04139256191d549981c71d5578e324f8c410c26b
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-05-31 20:54:31 +0300

    feat: add code for generating Pascal's Triangle
    This commit adds the code for generating Pascal's Triangle of n rows. The `pascal_triangle` function takes an integer `n` as input and returns the Pascal's Triangle as a list of lists. The function uses nested loops to calculate the values of each element in the triangle.
    
    The commit also includes the addition of the README.md file for the 0x00-pascal_triangle directory.
    
    Note: The commit message has been modified to provide more context and explanation. The actual commit message should be a concise summary without any additional details.

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''generates pascal's triangle of n rows'''
+
+
+def pascal_triangle(n):
+    '''generates pascal's triangle of n rows'''
+    triangle = []
+    if n <= 0:
+        return triangle
+    else:
+        for i in range(n):
+            row = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            triangle.append(row)
+        return triangle
