7c6725401d98bc321faac56faa281d0388d57b7f
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-06-30 19:20:38 +0300

    refactor: Improve UTF-8 validation code

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -11,19 +11,17 @@ def validUTF8(data) -> bool:
     """
     num_bytes = 0
     for byte in data:
-        if num_bytes == 0:
-            if byte >> 7 == 0:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
                 continue
-            elif byte >> 5 == 0b110:
-                num_bytes = 1
-            elif byte >> 4 == 0b1110:
-                num_bytes = 2
-            elif byte >> 3 == 0b11110:
-                num_bytes = 3
-            else:
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
             if byte >> 6 != 0b10:
                 return False
-            num_bytes -= 1
+        num_bytes -= 1
     return num_bytes == 0
